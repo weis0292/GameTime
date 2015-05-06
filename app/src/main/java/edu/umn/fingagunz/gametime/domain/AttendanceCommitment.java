37ac6de9c320e53bb9c6a53694fcb5e851d73c8a
Mike Weispfenning <weis0292@umn.edu>
2015-05-05 22:36:24 -0500

    Random changes for James

---
+++ app/src/main/java/edu/umn/fingagunz/gametime/domain/AttendanceCommitment.java
@@ -0,0 +1,32 @@
+package edu.umn.fingagunz.gametime.domain;
+
+import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+
+/**
+ * Created by Mike on 5/5/2015.
+ */
+@ParseClassName("AttendanceCommitment")
+public class AttendanceCommitment extends ParseObject
+{
+//	public Player getPlayer()
+//	{
+//		Player player = null;
+//		try { player = getParseObject("player").fetchIfNeeded(); }
+//		catch (ParseException ex) { }
+//		return player;
+//	}
+//	public void setPlayer(Player player) { put("player", player); }
+//
+//	public Game getGame()
+//	{
+//		Game game = null;
+//		try { game = getParseObject("game").fetchIfNeeded(); }
+//		catch (ParseException ex) { }
+//		return game;
+//	}
+//	public void setGame(Game game) { put("game", game); }
+//
+//	public
+}
