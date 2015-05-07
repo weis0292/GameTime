7aadc1303a849997488d501b74a4c517979fda6a
Mike Weispfenning <weis0292@umn.edu>
2015-05-06 21:28:10 -0500

    Got most of the Upcoming Games page working, still need to navigate to the GameDetail view when a game is clicked.

--- app/src/main/java/edu/umn/fingagunz/gametime/parse/queryadapter/TeamsParseQueryAdapter.java
+++ app/src/main/java/edu/umn/fingagunz/gametime/parse/queryadapter/TeamsParseQueryAdapter.java
@@ -15,45 +15,45 @@ import edu.umn.fingagunz.gametime.R;
 /**
  * Created by Mike on 4/30/2015.
  */
-public class TeamsParseQueryAdapter extends ParseQueryAdapter<ParseObject>
-{
-	public TeamsParseQueryAdapter(Context context, final ParseObject player)
-	{
-		super(context, new QueryFactory<ParseObject>()
-		{
-			@Override
-			public ParseQuery<ParseObject> create()
-			{
-				//String name = player.getString("playerName");
-				ParseQuery<ParseObject> query = ParseQuery.getQuery("TeamMember");
-				query.whereEqualTo("player", player);
-				return query;
-			}
-		});
-	}
-
-	@Override
-	public View getItemView(ParseObject object, View v, ViewGroup parent)
-	{
-		if (v == null)
-		{
-			v = View.inflate(getContext(), R.layout.activity_teams_row, null);
-		}
-
-		super.getItemView(object, v, parent);
-
-		try
-		{
-			ParseObject team = object.getParseObject("team");
-			String teamName = team.fetchIfNeeded().getString("teamName");
-			((TextView)v.findViewById(R.id.teams_row_name_label)).setText(teamName);
-		}
-		catch(ParseException ex)
-		{
-			// I have absolutely no idea what to do here?!?!?!
-		}
-
-
-		return v;
-	}
-}
+//public class TeamsParseQueryAdapter extends ParseQueryAdapter<ParseObject>
+//{
+//	public TeamsParseQueryAdapter(Context context, final ParseObject player)
+//	{
+//		super(context, new QueryFactory<ParseObject>()
+//		{
+//			@Override
+//			public ParseQuery<ParseObject> create()
+//			{
+//				//String name = player.getString("playerName");
+//				ParseQuery<ParseObject> query = ParseQuery.getQuery("TeamMember");
+//				query.whereEqualTo("player", player);
+//				return query;
+//			}
+//		});
+//	}
+//
+//	@Override
+//	public View getItemView(ParseObject object, View v, ViewGroup parent)
+//	{
+//		if (v == null)
+//		{
+//			v = View.inflate(getContext(), R.layout.activity_teams_row, null);
+//		}
+//
+//		super.getItemView(object, v, parent);
+//
+//		try
+//		{
+//			ParseObject team = object.getParseObject("team");
+//			String teamName = team.fetchIfNeeded().getString("teamName");
+//			((TextView)v.findViewById(R.id.teams_row_name_label)).setText(teamName);
+//		}
+//		catch(ParseException ex)
+//		{
+//			// I have absolutely no idea what to do here?!?!?!
+//		}
+//
+//
+//		return v;
+//	}
+//}
