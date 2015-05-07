219ae9a54f829f6a3c786f5b86f2f36680039e11
Mike Weispfenning <weis0292@umn.edu>
2015-05-07 16:54:52 -0500

    Added something

--- app/src/main/java/edu/umn/fingagunz/gametime/GameDetailActivity.java
+++ app/src/main/java/edu/umn/fingagunz/gametime/GameDetailActivity.java
@@ -22,6 +22,7 @@ public class GameDetailActivity extends Activity
 	final private static int REPLY_DECLINE = 0x02;
 
 	private AttendanceCommitment attendanceCommitment;
+	private Game game = new Game();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -59,7 +60,6 @@ public class GameDetailActivity extends Activity
 		Intent intent = getIntent();
 		String gameId = intent.getStringExtra("gameId");
 		String playerId = intent.getStringExtra("currentUserId");
-		Game game = new Game();
 		game.setObjectId(gameId);
 		try { game.fetchIfNeeded(); }
 		catch (ParseException e) { e.printStackTrace(); }
@@ -92,9 +92,9 @@ public class GameDetailActivity extends Activity
 
 	private void setReplyIcons(int reply)
 	{
-		getAcceptImageView().setImageResource(reply == REPLY_ACCEPT ? R.mipmap.ic_thumb_up_black_24dp : R.mipmap.ic_thumb_up_grey600_24dp);
-		getMaybeImageView().setImageResource(reply == REPLY_MAYBE ? R.mipmap.ic_thumbs_up_down_black_24dp : R.mipmap.ic_thumbs_up_down_grey600_24dp);
-		getDeclineImageView().setImageResource(reply == REPLY_DECLINE ? R.mipmap.ic_thumb_down_black_24dp : R.mipmap.ic_thumb_down_grey600_24dp);
+		getAcceptImageView().setImageResource(reply == REPLY_ACCEPT ? R.mipmap.ic_thumb_up_black_36dp : R.mipmap.ic_thumb_up_grey600_36dp);
+		getMaybeImageView().setImageResource(reply == REPLY_MAYBE ? R.mipmap.ic_thumbs_up_down_black_36dp : R.mipmap.ic_thumbs_up_down_grey600_36dp);
+		getDeclineImageView().setImageResource(reply == REPLY_DECLINE ? R.mipmap.ic_thumb_down_black_36dp : R.mipmap.ic_thumb_down_grey600_36dp);
 	}
 
 	private void setReply(int reply)
@@ -135,6 +135,9 @@ public class GameDetailActivity extends Activity
 		switch (item.getItemId())
 		{
 			case R.id.action_edit:
+				Intent intent = new Intent(this, AddEditGameActivity.class);
+				intent.putExtra("GameObjectId", game.getObjectId());
+				startActivity(intent);
 				return true;
 		}
 
